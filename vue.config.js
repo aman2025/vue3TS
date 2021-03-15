e34d513a6507d46cd3c8401e6000a6ed1aa7b678
aman2025 <2247367564@qq.com>
2021-03-15 14:40:34 +0800

    vue3-typescript初始

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devServer: {
+    hot: true,
+    port: 8005,
+    open: true,
+    noInfo: false,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  }
+  //
+};
